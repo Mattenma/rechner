fcd24c1a350189eeca9dda4355c2968d06a254b1
Mattenma <matthiasmattenberger@gmail.com>
2024-05-18 11:15:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <title></title>
-  <meta name="Generator" content="Cocoa HTML Writer">
-  <meta name="CocoaVersion" content="2487.4">
-  <style type="text/css">
-    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica}
-    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; min-height: 14.0px}
-  </style>
-</head>
-<body>
-<p class="p1">self.addEventListener('install', (e) =&gt; {</p>
-<p class="p1"><span class="Apple-converted-space">  </span>e.waitUntil(</p>
-<p class="p1"><span class="Apple-converted-space">    </span>caches.open('rendite-schaetzer').then((cache) =&gt; cache.addAll([</p>
-<p class="p1"><span class="Apple-converted-space">      </span>'/',</p>
-<p class="p1"><span class="Apple-converted-space">      </span>'/index.html',</p>
-<p class="p1"><span class="Apple-converted-space">      </span>'/sheet.css',</p>
-<p class="p1"><span class="Apple-converted-space">      </span>'/manifest.json',</p>
-<p class="p1"><span class="Apple-converted-space">      </span>'/icon-192x192.png',</p>
-<p class="p1"><span class="Apple-converted-space">      </span>'/icon-512x512.png'</p>
-<p class="p1"><span class="Apple-converted-space">    </span>])),</p>
-<p class="p1"><span class="Apple-converted-space">  </span>);</p>
-<p class="p1">});</p>
-<p class="p2"><br></p>
-<p class="p1">self.addEventListener('fetch', (e) =&gt; {</p>
-<p class="p1"><span class="Apple-converted-space">  </span>e.respondWith(</p>
-<p class="p1"><span class="Apple-converted-space">    </span>caches.match(e.request).then((response) =&gt; response || fetch(e.request)),</p>
-<p class="p1"><span class="Apple-converted-space">  </span>);</p>
-<p class="p1">});</p>
-</body>
-</html>
+const CACHE_NAME = 'rendite-schaetzer-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
